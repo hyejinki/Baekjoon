3ccfdea70ca6bba04555357195bb9fd7820ab16c
Lim Byung Kuk <lbk5770@gmail.com>
2023-01-31 17:58:26 +0900

    dfd

--- stage_11/25501_재귀의귀재.py
+++ stage_11/25501_재귀의귀재.py
@@ -1,8 +1,28 @@
 # 팰린드롬인지 판별하는 함수를 만들고 더불어 판별하는 과정에서 recursion 함수를 몇 번 호출하는지 셀 것이다.
-def isPalindrome(string):
-    reversed_str = string[::-1]
-    if string == reversed_str:
-        return 1
-    else:
-        return 0
+# def isPalindrome(string):
+#     i = 0
+#     reversed_str = string[::-1]
+#     if string == reversed_str:
+#         return 1
+#     else:
+#         return 0
+#     isPalindrome()
 
+def recursion(s, l, r):
+    if l >= r: 
+        return 1
+    elif s[l] != s[r]: 
+        return 0
+    else: 
+        return recursion(s, l+1, r-1)
+
+def isPalindrome(s):
+    return recursion(s, 0, len(s)-1)
+
+print('ABBA:', isPalindrome('ABBA'))
+print('ABC:', isPalindrome('ABC'), )
+
+
+# T = int(input())
+# for test_case in range(T):
+#     S = input()
