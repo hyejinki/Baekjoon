52be989b6604fe6adf6470bab8723c54faf9532c
hyejinki <zlagg23@gmail.com>
2023-02-02 23:09:42 +0900

    11:09

--- stage_10/18870_좌표압축.py
+++ stage_10/18870_좌표압축.py
@@ -21,22 +21,31 @@
 
 # 리스트 복사해서 하나 set하고 인덱스 번호 
 
-import copy
+# import copy
+# N = int(input())
+
+# ans = []
+# li_x = list(map(int, input().split()))      # 오리지널
+# li_copy = sorted(copy.deepcopy(li_x))       # 복사
+
+
+
+# for n in li_x:
+# #     count = 0
+# #     for i in set(li_copy):
+# #         if i < n:
+# #             count += 1
+# #     ans.append(count)            
+#     # N보다 작은 수.
+#     ans.append(list(set(li_copy)).index(n))
+
+# print(*ans)
+
+
 N = int(input())
-
+arr = list(map(int, input().split()))
+new_arr = sorted(list(set(arr))) # 중복 제거 -> 리스트화 -> 정렬
 ans = []
-li_x = list(map(int, input().split()))      # 오리지널
-li_copy = sorted(copy.deepcopy(li_x))       # 복사
-
-
-
-for n in li_x:
-#     count = 0
-#     for i in set(li_copy):
-#         if i < n:
-#             count += 1
-#     ans.append(count)            
-    # N보다 작은 수.
-    ans.append(list(set(li_copy)).index(n))
-
+for num in arr:
+    ans.append(new_arr.index(num))  # 정렬한 리스트의 인덱스 넘버 == 보다 작은 수의 개수
 print(*ans)
