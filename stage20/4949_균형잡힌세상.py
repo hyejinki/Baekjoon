699777870bbfa05addc92565792fa837a55a51bf
hyejinki <zlagg23@gmail.com>
2023-02-15 11:09:09 +0900

    11:09

--- stage20/4949_균형잡힌세상.py
+++ stage20/4949_균형잡힌세상.py
@@ -1,3 +1,23 @@
+import sys
 
-print(input())
+while True:
+    string = (sys.stdin.readline())
+    stack = [0]*100
+    top = -1
+    ans = 'yes'
+    for x in string:
+        if x == '(' or x == '[':
+            top += 1
+            stack[top] = x
+        if x == ')':
+            if top > -1:
+                if stack[top] ==  '(':
+                    top -= 1
+                else:
 
+        elif x == ']':
+            if stack[top] == '[':
+                top -= 1
+
+    if string == ['.']:
+        break
--- stage_13/1620_나는야포켓몬마스터이다솜.py
+++ stage_13/1620_나는야포켓몬마스터이다솜.py
@@ -2,12 +2,3 @@ N, M = map(int, input().split())
 arr1 = [input() for _ in range(N)]
 
 
-for _ in range(M):
-    dasom = input()
-    for j in range(N):
-        if '0' < dasom[0] and dasom[0] < '9':
-            if int(dasom) == j:
-                print(arr1[j-1])
-        else:
-            if dasom == arr1[j]:
-                print(j+1)
