f524ed5214dfb6372bea4346d401280fa2e390fc
hyejinki <zlagg23@gmail.com>
2023-02-22 11:27:55 +0900

    11:27

--- stage_21/5430_AC.py
+++ stage_21/5430_AC.py
@@ -2,15 +2,18 @@ import sys
 from collections import deque
 T = int(sys.stdin.readline())
 for _ in range(T):
-    p = sys.stdin.readline()            # 수행할 함수
+    p = str(sys.stdin.readline().strip())            # 수행할 함수
     n = int(sys.stdin.readline())       # 배열 크기
-    arr = deque(list(map(int, sys.stdin.readline().strip('[]\n'+'').split(','))))
-   
+    arr = deque(sys.stdin.readline().strip('[').strip().strip(']').split(','))
+    print(type(arr))
     for i in range(len(p)):
         if p[i] == 'R':     # 순서 뒤집기
-            last = arr[-1]  # arr 마지막 숫자 기억하고
-            while last != arr[0]:   # 마지막 숫자가 arr 맨 앞에 올 때까지
-                arr.append(arr.popleft())   # 왼쪽으로 shift
+            arr.reverse()
+    
         else:               # 맨 앞 삭제
-            arr.popleft()
-    print(arr)
+            if len(arr) ==1:
+                arr = 'error'  
+              
+            else:
+                arr.popleft()
+    print('['+','.join(arr)+ ']')
