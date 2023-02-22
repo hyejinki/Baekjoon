e8d173b66bc2cd55e7d091b35d112e307d316777
hyejinki <zlagg23@gmail.com>
2023-02-23 08:50:02 +0900

    8:49

--- stage_20/1874_스택수열.py
+++ stage_20/1874_스택수열.py
@@ -1,18 +1,16 @@
 n = int(input())
 stack = []
-ans = ''
-val = 0                                 # 현재값
+str = ''
 for _ in range(n):
-    num = int(input())
-    if num > val:                       # 입력값이 현재값보다 크면
-        for i in range(1, num + 1):
-            stack.append(i)             # push
-            ans += '+'
-            val = stack.pop(num)        # val = 현재값 
-    else:
-        val = stack.pop(num)
-    
+    x = int(input())
     
 
-    
 
+
+
+'''
+8
+1 2 3 4 5 6 7 8
+
+
+'''
