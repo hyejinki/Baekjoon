b2213438f60ca8e8e8544491943ed217e5665a9b
Kim Hye Jin <zlagg23@gmail.com>
2023-02-23 15:49:27 +0900

    3:49

--- stage_20/1874_스택수열.py
+++ stage_20/1874_스택수열.py
@@ -1,10 +1,22 @@
+import sys
 n = int(input())
 stack = []
-str = ''
+ans = ''
 for _ in range(n):
-    x = int(input())
-    
+    x = int(sys.stdin.readline())                
+    for i in range(1, x+1):          # 스택 비어있으면
+        stack.append(i)
+        print('+')
+    while ans != x:                             # x를 pop할 때까지
+        if x > stack[-1]:                       # x가 스택 마지막 요소보다 크면
+            for i in range(stack[-1] + 1, x+1): #  push
+                stack.append(i)
+            stack.pop()
+        else:                           # 작거나 같으면
+            while stack[-1] != x:
+                ans = stack.pop()
 
+print(ans)
 
 
 
