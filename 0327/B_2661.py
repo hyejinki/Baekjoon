c43fec84baf115415bfe340c8e444a3ce28651bf
hyejinki <zlagg23@gmail.com>
2023-03-28 23:20:05 +0900

    11:20

--- 0327/B_2661.py
+++ 0327/B_2661.py
@@ -3,10 +3,36 @@
 
 
 def f(i):       # i 는 리스트 길이
-    k = 0
-    x = i - 1  # 인덱스 번호
-    for t in range(2, i/2):
-        while k != i / 2:
-            if ans[x - t - k] == ans[x - k]:
-                k += 1
 
+    
+    for t in range(2, i//2 + 1):
+            # if ans[i][i - t - k - 1] == ans[i][i - k - 1]:
+        if i-(2*t)-1 < 0:
+            a = ans[i][i-1-t::-1]
+        else:
+            a = ans[i][i-1-t:i-(2*t)-1:-1]
+        if ans[i][i-1:i-t-1:-1] == a:
+            return 0
+    return 1
+    
+N = (int(input()))
+ans = ['0'] * 81
+num = [i for i in range(81)]
+ans[1] = '1'
+ans[2] = '12'
+ans[3] = '121'
+if N > 3:
+    x = 4
+    i = 1
+    while x <= N:
+        if num[i] != int(ans[x-1][-1]):
+            ans[x] = ans[x-1] + str(num[i])
+            if f(x) == 1:
+                x += 1
+                i = 1
+            else:
+                i += 1
+        else:
+            i += 1
+
+print(int(ans[N]))
