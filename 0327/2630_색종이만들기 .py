3426ea7e0cead9c082c56e665a2401eed61a2955
Hyejinki <zlagg23@gmail.com>
2023-03-29 17:54:44 +0900

    5:54

--- 0327/2630_색종이만들기 .py
+++ 0327/2630_색종이만들기 .py
@@ -1,10 +1,13 @@
 
+def check(si, ei, sj, ej):
+    pass
 
 
-
-
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
 
 check(0, N // 2, 0, N//2)
 check(0, N // 2, N // 2, N)
 check( N // 2, N, 0, N//2)
 check( N // 2, N,  N // 2, N)
+
