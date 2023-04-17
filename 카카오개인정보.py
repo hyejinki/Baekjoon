576f56c50278c44b1f071e1f7e4b159037139cab
HyejinKim <zlagg23@gmail.com>
2023-04-17 17:51:47 +0900

    5:51

--- 카카오개인정보.py
+++ 카카오개인정보.py
@@ -1,16 +1,10 @@
-a = ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]
-# print(a[0][-1])
 
-
-b = []
-b = list(map(str, a[0].split('.')))
-print(b)
-# for x in b[0:2]:
+privacies = ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]
+p = len(privacies)
+date = []
+for x in privacies:
+    print(x)
+    date.append(x.replace(' ', '.').split('.'))
     
 
-print(25 % 12)
-
-
-t = ["A 6", "B 12", "C 3"]
-for y in range(len(t)):
-    
+print(date)
