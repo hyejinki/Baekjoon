f85037bcb406019a076d3690cd4b5a0f63c87d36
hyejinki <zlagg23@gmail.com>
2023-04-24 19:01:16 +0900

    d

--- 카카오개인정보.py
+++ 카카오개인정보.py
@@ -1,10 +1,30 @@
 
-privacies = ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]
+privacies = ["2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"]
+terms = ["Z 3", "D 5"]
+today = "2020.01.01"
 p = len(privacies)
+t = len(terms)
 date = []
+term = []
+ans = []
+today = today.split('.')
 for x in privacies:
-    print(x)
-    date.append(x.replace(' ', '.').split('.'))
-    
-
-print(date)
+    date.append(x.replace(' ', '.').split('.')) 
+for x in terms:
+    term.append(x.split(' '))
+for i in range(p):
+    for j in range(t):
+        if date[i][-1] == term[j][0]:
+            div = (int(term[j][1]) + int(date[i][1])) // 12
+            mod = (int(term[j][1]) + int(date[i][1])) % 12
+            year = int(date[i][0]) + div
+            month = mod
+            if int(today[0]) > year \
+                or int(today[0]) == year and int(today[1]) > month\
+                or (int(today[0]) == year and int(today[1]) == month\
+                and int(today[2]) >= int(date[i][2])):
+                ans.append(i+1)
+            break
+            
+print(ans)
+# print(date)
