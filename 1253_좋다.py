d0d7c1ff5bfd1fb2a5fa3917e9856fe70279e72b
hyejinki <zlagg23@gmail.com>
2023-04-24 21:15:43 +0900

    9:15

---
+++ 1253_좋다.py
@@ -0,0 +1,23 @@
+import sys, copy
+
+N = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().split()))
+arr.sort()
+
+cnt = 0
+for x in arr:
+    temp = copy.deepcopy(arr)
+    temp.remove(x)
+
+    i = 0
+    j = N - 2
+    while i < j and j < N - 1:
+        if temp[i] + temp[j] == x:
+            cnt += 1
+            i += 1
+            break
+        elif temp[i] + temp[j] < x:
+            i += 1
+        elif temp[i] + temp[j] > x:
+            j -= 1
+print(cnt)
