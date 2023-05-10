36f3f5b1a715c42ef9d9684dc11bad819333db0f
hyejinki <zlagg23@gmail.com>
2023-05-10 17:15:02 +0900

    5

--- 15week/1806_부분합.py
+++ 15week/1806_부분합.py
@@ -1,18 +1,40 @@
 # 10 15
 # 5 1 3 5 10 7 4 9 2 8
 
+
 N, S = map(int, input().split())
 arr = list(map(int, input().split()))
 
-head, tail = 0, 1
-min = N
+head = 0
+min = N + 1
 
-while head < N:
-    if head >= S:
-        pritn(1)
+for x in arr:
+    if x >= S:
+        print(1)
         break
+else:
+    while head < N:
+        # if arr[head] >= S:   # 처음부터 넘어가면 1 출력하고 
+        #     print(1)
+        #     break
+        # else:
+            tail = head + 1
+            sum = arr[head]
+            while tail < N:
+                sum += arr[tail]
+                tail += 1
+                if sum >= S:
+                    if min > (tail - head):
+                        min = (tail - head)
+                    break
+            if min == 2:
+                break
+            head += 1
+    
         
+    
+    if min == N + 1:
+        print(0)
     else:
-        sum = 0
-        while sum < S:
-            
+        print(min)
+
